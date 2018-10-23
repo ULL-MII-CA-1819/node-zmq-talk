ce98dbc5f8e4f7caf82490796826a5ceac11ca4c
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2018-10-23 11:37:29 +0100

    zmq-time-rep-cluster.js: require(os).cpus().length loop

--- zmq-time-rep-cluster.js
+++ zmq-time-rep-cluster.js
@@ -1,15 +1,20 @@
 'use strict';
+const inspect = require("util").inspect;
+const ins = (x) => inspect(x, {depth: Math.infinity});
+const ic = (x) => console.log(ins(x));
+
+
 const
   cluster = require('cluster'),
   zmq = require('zeromq'),
 
-  workerCount = 3,
-
+  workerCount = require('os').cpus().length,
   externalEndpoint = 'tcp://127.0.0.1:5433',
   workerEndpoint = 'ipc://filer-dealer.ipc';
 
 if (cluster.isMaster) {
 
+  ic(require('os').cpus());
   // Master process - create ROUTER and DEALER sockets, bind endpoints.
   let
     router = zmq.socket('router').bind(externalEndpoint),
